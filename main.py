c9318455c7ecb4fb9b6dc96c24a65d3bd3549ee3
impulseweb3 <impulseweb3@gmail.com>
2025-09-05 17:34:58 +0200

    Integrate dotenv for environment variable management, add proxy support to Emailnator and Procap, and implement email verification workflow with Twitter.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import asyncio
 
+import dotenv
+
+from emailnator import Emailnator
+from procap import Procap
 from twitter.twitter import Twitter
 
 
 async def main() -> None:
-    twitter = Twitter()
+    dotenv.load_dotenv()
+
+    proxy = 'http://qxwltgrb:gxoxa8007tc7@23.95.150.145:6114'
+
+    twitter = Twitter(proxy)
+
+    emailnator = Emailnator(proxy)
+    await emailnator.fetch_cookies()
+
+    procap = Procap()
 
     await twitter.guest_activate()
     print(twitter.guest_token)
@@ -12,6 +25,27 @@ async def main() -> None:
     await twitter.flow_name_signup()
     print(twitter.flow_token)
 
+    email = str()
+    valid = False
+    while not valid:
+        email = await emailnator.get_email()
+        valid = await twitter.email_available(email)
+    print(email)
 
-if __name__ == "__main__":
+    await twitter.begin_verification(email, 'John Doe')
+    print('begin_verification')
+
+    code = None
+    while not code:
+        code = await emailnator.get_code(email)
+    print(code)
+
+    access_token = await procap.get_token(proxy)
+    print(access_token)
+
+    await twitter.email_verification('John Doe', email, access_token, code)
+    print(twitter.flow_token)
+
+
+if __name__ == '__main__':
     asyncio.run(main())
